e4cab2f96673e45987161e352e858b434769a6f4
adajic <adajic@awesomemotive.com>
2024-02-15 16:57:13 +0100

    .

--- app.py
+++ app.py
@@ -31,12 +31,54 @@ def handleRequestAll():
 		if request.method == "POST":
 			data = request.get_json()
 			
+			# Fill pdf, and fill output with pdf bytes
+			reader = PdfReader("PURCHASE_INVOICE_EMAIL_TEMPLATE.pdf")
+			writer = PdfWriter()
+			page = reader.pages[0]
+			fields = reader.get_fields().keys()
+			writer.add_page(page)
+
+			field_values = [""] * 13
+			field_values[0] = "0"
+			field_values[1] = "1"
+			field_values[2] = "2"
+			field_values[3] = "3"
+			field_values[4] = "4"
+			field_values[5] = "5"
+			field_values[6] = "6"
+			field_values[7] = "7"
+			field_values[8] = "8"
+			field_values[9] = "9"
+			field_values[10] = "10"
+			field_values[11] = "11"
+			field_values[12] = "12"			
+
 			output_fields = {				
 				"pdf": "",
 				"image": ""
 			}
-			output_fields["pdf"] = "pdfBytes"
-			output_fields["image"] = "imageBytes"
+			
+			num = 0
+			for field in fields:	
+				writer.update_page_form_field_values(
+					writer.pages[0], {field: field_values[num]}
+				)
+				num = num + 1
+
+			bytes_to_send = io.BytesIO()
+			writer.write(bytes_to_send)
+			bytes_to_send.seek(0)			
+			output_fields["pdf"] = list(bytes_to_send.read())
+			
+			bytes_to_send.seek(0)
+			pdf = pypdfium2.PdfDocument(bytes_to_send)
+			page = pdf.get_page(0)
+			pil_image = page.render_topil(scale=3)
+			img_byte_arr = io.BytesIO()
+			pil_image.save(img_byte_arr, format='JPEG', quality=100, subsampling=0)
+			img_byte_arr.seek(0)	
+			page.close()
+			output_fields["image"] = list(img_byte_arr.read())			
 			
 			output = {
 				"error_msg": "",
